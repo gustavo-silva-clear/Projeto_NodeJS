793ff0643f5fca8a209dd1a31df016e700d1adc8
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-24 11:34:17 -0300

    feat: nodemon and creating server with express

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
+
+app.get('/',(req, res) => {
 
     console.log("URL:", req.url);
     console.log("METHOD", req.method);
 
-    switch (req.url) {
-
-        case '/':
             res.statusCode = 200;
             res.setHeader("Content-type", "text/html")
             res.end('<h1>Hi</h1>');
 
-            break;
-
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader("Content-type", "application/json");
-            res.end(JSON.stringify({
-
-            users:[{
-
-                name:'dev.dev',
-                email:'dev.dev@gmail.com',
-                id: 1
-
-            }]
-                
-            }));
- 
-    }
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users',(req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.json({
+
+    users:[{
+
+        name:'dev.dev',
+        email:'dev.dev@gmail.com',
+        id: 1
+
+    }]
+        
+    });
+
+});
+
+
+app.listen(3000, '127.0.0.1', () => {
 
     console.log("server running!");
 
