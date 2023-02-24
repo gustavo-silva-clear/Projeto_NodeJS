5a8377956f04573edcca999ad90e1035bc640d0b
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-24 11:00:42 -0300

    feat: detecting different url

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const http = require('http');
 
-let server = http.createServer((req, res)=>{
+let server = http.createServer((req, res) => {
 
-console.log("URL:", req.url);
-console.log("METHOD", req.method);
+    console.log("URL:", req.url);
+    console.log("METHOD", req.method);
 
+    switch (req.url) {
 
-res.end('ok');
+        case '/':
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/html")
+            res.end('<h1>Hi</h1>');
+
+            break;
+
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader("Content-type", "application/json");
+            res.end(JSON.stringify({
+
+            users:[{
+
+                name:'dev.dev',
+                email:'dev.dev@gmail.com',
+                id: 1
+
+            }]
+                
+            }));
+ 
+    }
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+server.listen(3000, '127.0.0.1', () => {
 
-console.log("server running!");
+    console.log("server running!");
 
 });
