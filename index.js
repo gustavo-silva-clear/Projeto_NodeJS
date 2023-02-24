d5e67c0d1766711c88d3d5e6ed20521a74a6a5aa
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-24 10:41:44 -0300

    feat: server running and showing the methods

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world!!!")
+const http = require('http');
+
+let server = http.createServer((req, res)=>{
+
+console.log("URL:", req.url);
+console.log("METHOD", req.method);
+
+
+res.end('ok');
+});
+
+server.listen(3000, '127.0.0.1', ()=>{
+
+console.log("server running!");
+
+});
