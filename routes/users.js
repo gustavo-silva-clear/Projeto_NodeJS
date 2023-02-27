ddd822efcfa204c015c8d76a08225d07f131d3fd
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-27 09:24:40 -0300

    feat: editing user via put

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ module.exports = app => {
 
     let route = app.route('/users');
 
-
+//Pega os dados do banco
     route.get((req, res) => {
 
         db.find({}).sort({ name: 1 }).exec((err, users) => {
@@ -32,6 +32,7 @@ module.exports = app => {
 
     });
 
+//Recebe os dados do banco
     route.post((req, res) => {
 
         db.insert(req.body, (err, user) => {
@@ -46,5 +47,44 @@ module.exports = app => {
             }
         });
     });
+
+//Pego um usuario especifico
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+
+        db.findOne({ _id: req.params.id }).exec((err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            }
+            else {
+
+                res.status(200).json(user);
+
+            }
+
+        });
+
+    });
+
+    //modifico as informações de um usuario
+    routeId.put((req, res) => {
+
+        db.update({ _id: req.params.id } , req.body , err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            }
+            else {
+
+                res.status(200).json(Object.assign(req.params , req.body));
+
+            }
+
+        });
+
+    });
+
 
 };
