ca8ae444c39bdb146315e364098edbf24e1b4fa5
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-27 09:30:25 -0300

    feat: deleting user via delete

--- routes/users.js
+++ routes/users.js
@@ -86,5 +86,23 @@ module.exports = app => {
 
     });
 
+    routeId.delete((req , res)=>{
+
+        db.remove({_id: req.params.id} , {} , err =>{
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            }
+            else {
+
+                res.status(200).json(req.params);
+
+            }
+
+
+        });
+
+    });
+
 
 };
